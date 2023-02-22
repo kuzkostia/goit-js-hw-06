21b0709c58b3a7b375c7751f66781cf7990e08b5
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-02-22 19:04:18 +0200

    Summary

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,11 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
 const handleInput = (event) => {
-    if (event.currentTarget.value.trim() !== "") {
-        nameOutput.textContent = event.currentTarget.value;
-    }
-    else {
-        nameOutput.textContent = 'Anonymous';
-    }
-}
-nameInput.addEventListener('input', handleInput);
+  if (event.currentTarget.value.trim() !== "") {
+    nameOutput.textContent = event.currentTarget.value;
+  } else {
+    nameOutput.textContent = "Anonymous";
+  }
+};
+nameInput.addEventListener("input", handleInput);
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontSizeControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
+text.style.fontSize = fontSizeControl.value + 'px';
+
 fontSizeControl.addEventListener('input', (event) => {
     text.style.fontSize = event.currentTarget.value + 'px';
 })
